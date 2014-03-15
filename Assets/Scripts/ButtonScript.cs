852dd6444e4b36510bb81fc44555018d73391deb
AmpRack <rob.hill@gmail.com>
2014-03-14 19:13:05 -0700

    Menu Skeleton 2
    Updated button models. Scripting is still a scrappy pile of not-working.

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	// Store all camera points as Vectors
+	Vector3 posTitle;
+	Vector3 posMain;
+	Vector3 posSP;
+	Vector3 posMP;
+	Vector3 posOptions;
+	public Vector3 posDest;			// This is a placeholder vector for "where are we going next"
+	public Vector3 posNow; 			// Only holds transform.position
+
+	// Define which materials to use when the button is hovered over
+	public Material buttonOn;
+	public Material buttonOff;
+
+	// Detect all possible Main Menu buttons
+	GameObject startButton;
+	GameObject singleplayerButton;
+	GameObject multiplayerButton;
+	GameObject optionsButton;
+	GameObject quitgameButton;
+	GameObject backButton1;
+	GameObject backButton2;	
+	GameObject backButton3;
+	GameObject mainCamera;
+
+	enum MenuState {
+		Title = 0,
+		Main = 1,
+		Singleplayer = 2,
+		Multiplayer = 3,
+		Options = 4,
+	}
+	private MenuState _activeMenu;
+	private MenuState _destMenu;
+
+	public static bool isMoving;
+//	public float moveSpeed = 20.0f;;
+//	private Vector3 buttonPosOff;
+//	private Vector3 buttonPosOn;
+
+	void Start () {
+		// Find all button objects and define them
+		startButton = GameObject.Find("ButtonStart");
+		singleplayerButton = GameObject.Find("ButtonSP");
+		multiplayerButton = GameObject.Find("ButtonMP");
+		optionsButton = GameObject.Find("ButtonOpts");
+		quitgameButton = GameObject.Find("ButtonQuit");
+		backButton1 = GameObject.Find("ButtonBack1");
+		backButton2 = GameObject.Find("ButtonBack2");
+		backButton3 = GameObject.Find("ButtonBack3");
+		mainCamera = GameObject.Find("Main Camera");
+		// Find all empty CamLocation positions and define them
+		posTitle = GameObject.Find("TitleScreen").transform.position;
+		posMain = GameObject.Find("MainMenu").transform.position;
+		posSP = GameObject.Find("SinglePlayer").transform.position;
+		posMP = GameObject.Find("Multiplayer").transform.position;
+		posOptions = GameObject.Find("Options").transform.position;
+		posDest = GameObject.Find("TitleScreen").transform.position;
+//		buttonPosOff = transform.position;
+//		buttonPosOn = buttonPosOff;
+//		buttonPosOn.z = (buttonPosOff.z - 10);
+
+		_activeMenu = MenuState.Title;
+		_destMenu = MenuState.Title;
+
+		isMoving = false;
+
+		}
+	
+	void Update () {
+		DoCameraMoving ();
+	}
+
+	void OnMouseEnter() {
+		renderer.material = buttonOn;
+	}
+
+	void OnMouseExit() {
+		renderer.material = buttonOff;
+	}
+
+	void OnMouseDown() {
+		// transform.position = Vector3.MoveTowards(buttonPosOff, buttonPosOn, moveSpeed * Time.deltaTime);
+	}
+
+
+	void OnMouseUp() {
+		if (gameObject == startButton) {
+			Debug.Log("Clicked Start Button");
+			_destMenu = MenuState.Main;
+			posDest = posMain;
+		}
+		if (gameObject == singleplayerButton) {
+			Debug.Log("Clicked Single Player Button");
+			_destMenu = MenuState.Singleplayer;
+			posDest = posSP;
+		}
+		if (gameObject == multiplayerButton) {
+			Debug.Log("Clicked Multiplayer Button");
+			_destMenu = MenuState.Multiplayer;
+			posDest = posMP;
+		}
+		if (gameObject == optionsButton) {
+			Debug.Log("Clicked Options Button");
+			_destMenu = MenuState.Options;
+			posDest = posOptions;
+		}
+		if ((gameObject == backButton1)) {
+			Debug.Log("Clicked Back Button");
+			_destMenu = MenuState.Main;
+			posDest = posMain;
+		}
+		if ((gameObject == backButton2)) {
+			Debug.Log("Clicked Back Button");
+			_destMenu = MenuState.Main;
+			posDest = posMain;
+		}
+		if ((gameObject == backButton3)) {
+			Debug.Log("Clicked Back Button");
+			_destMenu = MenuState.Main;
+			posDest = posMain;
+		}
+		if (gameObject == quitgameButton) {
+			Debug.Log("Clicked QuitGame Button");
+			Application.Quit();
+
+		// transform.position = Vector3.MoveTowards(buttonPosOn, buttonPosOff, moveSpeed * Time.deltaTime);
+		}
+	}
+
+	// See if we should be changing pages, and move to it.
+	void DoCameraMoving(){
+		posNow = mainCamera.transform.position;
+		
+		if (_activeMenu != _destMenu) {
+			isMoving = true;
+//			mainCamera.transform.position = Vector3.MoveTowards(posNow, posDest, moveSpeed * Time.deltaTime);
+		}
+		if (posNow == posDest) {
+			isMoving = false;
+			_activeMenu = _destMenu;
+		}
+	}
+}
--- Assets/Scripts/CameraMenu.cs
+++ Assets/Scripts/CameraMenu.cs
@@ -1,31 +1,36 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class MenuMover : MonoBehaviour {
+public class CameraMenu : MonoBehaviour {
 
-	// Store all camera points as Vectors
+		// Store all camera points as Vectors
 	Vector3 posTitle;
 	Vector3 posMain;
 	Vector3 posSP;
 	Vector3 posMP;
 	Vector3 posOptions;
-	Vector3 posDest;			// This is a placeholder vector for "where are we going"
+	Vector3 posDest;			// This is a placeholder vector for "where are we going next"
 
 	public float moveSpeed = 0.0f;
 	private bool isMoving = false;
 
-	enum activeMenu {
+	enum Menu {
 		Title = 0,
 		Main = 1,
 		Singleplayer = 2,
 		Multiplayer = 3,
 		Options = 4,
+		QuitGame = 5,
 	}
-	private activeMenu _activeMenu;
-	private activeMenu _destMenu;
+	private Menu _activeMenu;
+	private Menu _destMenu;
+	private Menu _activeButton;
+	private Menu _destButton;
 
 	// Use this for initialization
 	void Start () {
+		// Find locations for all CamLocation.Empties, and assign values
+		// Also, default everything else to the Title Menu
 		posTitle = GameObject.Find("TitleScreen").transform.position;
 		posMain = GameObject.Find("MainMenu").transform.position;
 		posSP = GameObject.Find("SinglePlayer").transform.position;
@@ -33,65 +38,68 @@ public class MenuMover : MonoBehaviour {
 		posOptions = GameObject.Find("Options").transform.position;
 		posDest = GameObject.Find("TitleScreen").transform.position;
 		transform.position = posTitle;
-		_activeMenu = activeMenu.Title;
-		_destMenu = activeMenu.Title;
+		_activeMenu = Menu.Title;
+		_destMenu = Menu.Title;
+		_activeButton = Menu.Title;
+		_destButton = Menu.Title;
+		isMoving = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		MenuListener();
-		DoMoving();
+		DoMenuMoving();
 	}
 
 	// Listen for key-presses, then determine/assign the next menu page to load up.
 	void MenuListener() {
-		if ((_activeMenu == activeMenu.Title) && (Input.GetKey("down")) && (isMoving == false)) {
+		if ((_activeMenu == Menu.Title) && ((Input.GetKey("return")) || (Input.GetKey("enter"))) && (isMoving == false)) {
 			Debug.Log("Moving Title to Main");
-			_destMenu = activeMenu.Main;
+			_destMenu = Menu.Main;
 			posDest = posMain;
 		}
-		if ((_activeMenu == activeMenu.Main) && (Input.GetKey("left")) && (isMoving == false)) {
+		if ((_activeMenu == Menu.Main) && (Input.GetKey("left")) && (isMoving == false)) {
 			Debug.Log("Moving Main to MP");
-			_destMenu = activeMenu.Multiplayer;
+			_destMenu = Menu.Multiplayer;
 			posDest = posMP;
 		}
-		if ((_activeMenu == activeMenu.Main) && (Input.GetKey("right")) && (isMoving == false)) {
+		if ((_activeMenu == Menu.Main) && (Input.GetKey("right")) && (isMoving == false)) {
 			Debug.Log("Moving Main to SP");
-			_destMenu = activeMenu.Singleplayer;
+			_destMenu = Menu.Singleplayer;
 			posDest = posSP;
 		}
-		if ((_activeMenu == activeMenu.Main) && (Input.GetKey("down")) && (isMoving == false)) {
+		if ((_activeMenu == Menu.Main) && (Input.GetKey("down")) && (isMoving == false)) {
 			Debug.Log("Moving Main to Options");
-			_destMenu = activeMenu.Options;
+			_destMenu = Menu.Options;
 			posDest = posOptions;
 		}
-		if ((_activeMenu == activeMenu.Singleplayer) && (Input.GetKey("left")) && (isMoving == false)) {
+		if ((_activeMenu == Menu.Singleplayer) && (Input.GetKey("left")) && (isMoving == false)) {
 			Debug.Log("Moving SP to Main");
-			_destMenu = activeMenu.Main;
+			_destMenu = Menu.Main;
 			posDest = posMain;
 		}
-		if ((_activeMenu == activeMenu.Multiplayer) && (Input.GetKey("right")) && (isMoving == false)) {
+		if ((_activeMenu == Menu.Multiplayer) && (Input.GetKey("right")) && (isMoving == false)) {
 			Debug.Log("Moving MP to Main");
-			_destMenu = activeMenu.Main;
+			_destMenu = Menu.Main;
 			posDest = posMain;
 		}
 
-		if ((_activeMenu == activeMenu.Options) && (Input.GetKey("up")) && (isMoving == false)) {
+		if ((_activeMenu == Menu.Options) && (Input.GetKey("up")) && (isMoving == false)) {
 			Debug.Log("Moving Options to Main");
-			_destMenu = activeMenu.Main;
+			_destMenu = Menu.Main;
 			posDest = posMain;
 		}
 	}
 
 	// Read destination, then lerp to it
-	void DoMoving(){
-		Debug.Log("Made it to MenuMover");
+	void DoMenuMoving(){
+		Debug.Log("Made it to DoMenuMoving");
 		Vector3 posNow = transform.position;
 
 		if (_activeMenu != _destMenu) {
 			isMoving = true;
 			transform.position = Vector3.MoveTowards(posNow, posDest, moveSpeed * Time.deltaTime);
-			Debug.Log("Inside if-loop of doMoving");
+			Debug.Log("Inside if-loop of DoMoving");
 		}
 		if (posNow == posDest) {
 			isMoving = false;
@@ -99,4 +107,5 @@ public class MenuMover : MonoBehaviour {
 			Debug.Log("isMoving now set to false");
 		}
 	}
+
 }
