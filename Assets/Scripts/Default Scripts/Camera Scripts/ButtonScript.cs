99f17abfd32040bbfa1501a439aad48a753fa6de
AmpRack <rob.hill@gmail.com>
2014-03-17 00:56:12 -0700

    Menu Skeleton 3
    Button scripting is working and streamlined.

---
+++ Assets/Scripts/Default Scripts/Camera Scripts/ButtonScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	// Store all camera points as Vectors
+	public Vector3 posTitle;
+	public Vector3 posMain;
+	public Vector3 posSP;
+	public Vector3 posMP;
+	public Vector3 posOpts;
+	public Vector3 posNow;
+
+	// These are shared between all buttons
+	public Material buttonOn;
+	public Material buttonOff;
+	public float moveSpeed;
+
+	// Detect all possible Main Menu buttons
+	GameObject startButton;
+	GameObject singleplayerButton;
+	GameObject multiplayerButton;
+	GameObject optionsButton;
+	GameObject quitButton;
+	GameObject backButton1;
+	GameObject backButton2;	
+	GameObject backButton3;
+
+	void Awake() {
+		// Find all button objects and define them
+		startButton = GameObject.Find("ButtonStart");
+		singleplayerButton = GameObject.Find("ButtonSP");
+		multiplayerButton = GameObject.Find("ButtonMP");
+		optionsButton = GameObject.Find("ButtonOpts");
+		quitButton = GameObject.Find("ButtonQuit");
+		backButton1 = GameObject.Find("ButtonBack1");
+		backButton2 = GameObject.Find("ButtonBack2");
+		backButton3 = GameObject.Find("ButtonBack3");
+
+		// Find all empty CamLocation positions and define them
+		posTitle = GameObject.Find("TitleScreen").transform.position;
+		posMain = GameObject.Find("MainMenu").transform.position;
+		posSP = GameObject.Find("SinglePlayer").transform.position;
+		posMP = GameObject.Find("Multiplayer").transform.position;
+		posOpts = GameObject.Find("Options").transform.position;
+		posNow = posTitle;
+
+		Camera.main.transform.position = posNow;
+	}
+	
+	void Update() {
+	}
+}
---
+++ Assets/Scripts/Default Scripts/Camera Scripts/Button_Options.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button_Options : MonoBehaviour {
+
+	bool letsMove;				// Detects whether or not we're going between menus
+	ButtonScript buttons;		// Call the main script for variables
+	
+	void Start(){
+		buttons = GameObject.Find("ButtonHolder").GetComponent<ButtonScript>();
+		letsMove = false;
+	}
+	
+	void Update(){
+		DoMoving ();
+	}
+	
+	void OnMouseEnter(){
+		renderer.material = buttons.buttonOn;
+	}
+	
+	void OnMouseExit(){
+		renderer.material = buttons.buttonOff;
+	}
+	
+	void OnMouseUp(){
+		letsMove = true;
+	}
+	
+	void DoMoving(){
+		// If the assigned button was clicked, lock-out the other buttons and slide over to the next screen.
+		// Once it's done, unlock the buttons.
+		Camera.main.transform.position = buttons.posNow;
+		if ((letsMove == true) && (buttons.posNow != buttons.posOpts)) {
+			buttons.posNow = Vector3.MoveTowards(buttons.posNow, buttons.posOpts, (buttons.moveSpeed * Time.deltaTime));
+		}
+		
+		if ((letsMove) && (buttons.posNow == buttons.posOpts)) {
+			letsMove = false;
+		}
+	}
+}
